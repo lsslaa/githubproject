27374f3e1fbfdc7259e87db9d8af609a4d951b64
lsbrand <lsbrand@hebei.com>
2023-10-30 16:35:26 +0800

    master second commit

--- src/main/java/com/tianjin/ligong/gitHubTest01.java
+++ src/main/java/com/tianjin/ligong/gitHubTest01.java
@@ -3,5 +3,6 @@ package com.tianjin.ligong;
 public class gitHubTest01 {
     public static void main(String[] args) {
         System.out.println("ls");
+        System.out.println("ls2");
     }
 }
